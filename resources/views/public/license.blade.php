e031f49521e07fbbef0c16b9e25bc5aa50272b18
zawxtut <zawxtutaung@gmail.com>
2025-11-29 16:10:01 +0700

    mm

--- resources/views/public/license.blade.php
+++ resources/views/public/license.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>License Details - {{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+Myanmar:wght@400;600;700&display=swap');
 
